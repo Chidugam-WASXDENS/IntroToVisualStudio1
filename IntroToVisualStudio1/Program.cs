7c310c355d5aa5e6c70d145dbac95c5dd917e5f0
22cnjobuenwu <22cnjobuenwu@BRAVO-04>
2025-01-07 11:44:52 +0000

    Add project files.

---
+++ IntroToVisualStudio1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace IntroToVisualStudio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello");
+            Console.WriteLine("World!");
+        }
+    }
+}
